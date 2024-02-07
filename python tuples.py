54f184c75cbba21cf6af3a7dd17930fc87b36cdc
Keikaichu <156984902+Keikaichu@users.noreply.github.com>
2024-02-07 23:24:51 +0600

    Delete python tuples.py

--- python tuples.py
+++
@@ -1,8 +0,0 @@
-
-fruits = ("apple", "banana", "cherry")
-print(fruits[0])
-print(len(fruits))
-print(fruits[-1])
-
-fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-print(fruits[2:5])
