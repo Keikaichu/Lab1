49bc04c9e1fd90c2c343ae8c98c26547eabfa266
Keikaichu <156984902+Keikaichu@users.noreply.github.com>
2024-02-08 09:55:02 +0600

    Add files via upload

---
+++ lab3/6.py
@@ -0,0 +1,2 @@
+def my_function(**kid):
+  print("His last name is " + kid["lname"]) #If you do not know the number of keyword arguments that will be passed into your function, there is a prefix you can add in the function definition,
---
+++ lab3/9.py
@@ -0,0 +1,4 @@
+class MyClass:
+  x = 5
+
+p1 = MyClass()  #Create an object of MyClass called p1
